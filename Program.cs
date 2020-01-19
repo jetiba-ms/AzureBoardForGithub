b935adeae1e74cbba7fd809d735d03b784b5d32f
jetiba-ms <jessica.tibaldi@microsoft.com>
2020-01-19 15:41:26 +0100

    a new commit to test connection AB#189

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AzureBoardForGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, it's " +  DateTime.UtcNow.ToString("MM/dd/yyyy hh:mm:ss"));
         }
     }
 }
